5c18e79c5ed949390b90abccbab0d1bdb4ddaa82
mohamed200111 <mohakatalonya10@gmail.com>
2022-08-15 11:06:42 +0100

    First commit console rock-paper-scissors game

---
+++ index.js
@@ -0,0 +1,43 @@
+const getComputerChoice = () => {
+    const generateChoice = Math.floor(Math.random() * 3) + 1;
+    if (generateChoice === 1) {
+        return "rock";
+    } else if (generateChoice === 2) {
+        return "paper";
+    } else {
+        return "scissors";
+    }
+};
+
+const playRound = (playerSelection, computerSelection) => {
+    playerSelection = playerSelection.toLowerCase();
+
+    if (
+        (playerSelection === "scissors" && computerSelection === "rock") ||
+        (playerSelection === "paper" && computerSelection === "scissors") ||
+        (playerSelection === "rock" && computerSelection === "paper")
+    ) {
+        return "You Lose! Rock beats Scissors";
+    }
+    else if (
+        (playerSelection === "rock" && computerSelection === "scissors") ||
+        (playerSelection === "scissors" && computerSelection === "paper") ||
+        (playerSelection === "paper" && computerSelection === "rock")
+    ) {
+        return "You Win! Rock beats Scissors";
+    }
+    else if (playerSelection === computerSelection) {
+        return "Draw!";
+    }
+    else {
+        return "Invalid Input";
+    }
+};
+
+const game = () => {
+    for (let i = 0; i < 5; i++) {
+        const playerSelection = prompt();
+        const computerSelection = getComputerChoice();
+        console.log(playRound(playerSelection, computerSelection));
+    }
+};
